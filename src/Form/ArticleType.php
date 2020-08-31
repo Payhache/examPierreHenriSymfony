2fb96a5e83cd8d75d623df400c177720d501cb2b
payhache <payhache@gmail.com>
2020-08-31 14:46:21 +0200

    style

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +15,15 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content')
+            ->add('content', TextareaType::class,
+            [
+                'attr' => ['rows' => 15]
+            ])
             ->add('picture', FileType::class, 
             [
                 'mapped' => false,
                 'required' => false,
-                
+
             ])
         ;
     }
